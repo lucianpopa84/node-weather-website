a1cd7ead4a0eb5f5b23972b5647fd6b287432fbb
Popa Lucian <lucian.popa@iquestgroup.com>
2020-07-09 14:35:05 +0300

    setup app for heroku

--- src/app.js
+++ src/app.js
@@ -84,7 +84,7 @@ app.get('*', (req, res) => {
     });
 });
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Server is started on port ${port}`);
 });
